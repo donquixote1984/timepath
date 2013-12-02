de7545fffed47b899b72bb4094c525f90d940696
Ubuntu <ubuntu@ip-172-31-15-9.ap-southeast-1.compute.internal>
2013-12-02 13:44:51 +0000

    rails init

---
+++ timepath/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timepath::Application.config.secret_key_base = '5224df97c206652235bdf03e161b1c53848b7c70ef654c995539f681d89a3417863ef0483650e8d74ea322914158cdee4e296a8339d344671810799322b93e5e'
---
+++ timepath/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Timepath::Application.config.session_store :cookie_store, key: '_timepath_session'
